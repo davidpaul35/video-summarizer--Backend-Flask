49b48218fb97ea73fe842a0cad89003a38908734
davidpaul35 <78133877+davidpaul35@users.noreply.github.com>
2022-06-22 18:41:23 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,8 @@
+from flask import Flask #importing the module
+app=Flask(__name__) #instantiating flask object
+@app.route('/') #defining a route in the application
+def func(): #writing a function to be executed 
+       return 'PythonGeeks'
+if __name__=='__main__': #calling  main 
+       app.debug=True #setting the debugging option for the application instance
+       app.run() #launching the flask's integrated development webserver
