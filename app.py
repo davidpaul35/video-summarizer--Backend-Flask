0d2824fe62d9932496b62f27fdccf7067145907d
davidpaul35 <78133877+davidpaul35@users.noreply.github.com>
2022-10-22 09:43:10 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask #importing the module
 app=Flask(__name__) #instantiating flask object
 @app.route('/') #defining a route in the application
 def func(): #writing a function to be executed 
-       return 'PythonGeeks'
+       return 'True'
 if __name__=='__main__': #calling  main 
        app.debug=True #setting the debugging option for the application instance
        app.run() #launching the flask's integrated development webserver
