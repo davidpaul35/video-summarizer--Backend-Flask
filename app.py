2188890c39fe525096799260d867a2c515c3ee0c
davidpaul35 <78133877+davidpaul35@users.noreply.github.com>
2022-10-24 11:17:50 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,19 @@
-from flask import Flask #importing the module
-app=Flask(__name__) #instantiating flask object
-@app.route('/') #defining a route in the application
-def func(): #writing a function to be executed 
-       return 'True'
-if __name__=='__main__': #calling  main 
-       app.debug=True #setting the debugging option for the application instance
-       app.run() #launching the flask's integrated development webserver
+
+from flask import Flask,jsonify,request
+from main import*
+app = Flask(__name__)
+#@app.route('/summarize', methods=['GET','POST'])
+#def sample():
+#   if request.method == 'POST':
+#      return jsonify({ "message": "true"})  
+@app.route('/api/endpoint', methods=['GET', 'POST']) 
+def api_endpoint(): 
+    url = request.json['url'] 
+    res = main(url) 
+    return {'text': res} 
+
+if __name__ == '__main__':  
+    app.run(debug = True)
+
+ 
+
